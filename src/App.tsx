29d82e7aa89a1d1c34b26089341e27fefaf2d8d7
Maks Tkachyk <tkachykmakc@gmail.com>
2023-07-12 19:28:44 +0300

    Remove extra import

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import "./App.scss";
-import { useAppDispatch, useAppSelector } from "./hooks/redux";
-import { ProductSlice } from "./store/reducers/ProductSlice";
 import { ContentProvider } from "./UI/components/ContentProvider/ContentProvider";
 import Footer from "./UI/components/Footer/Footer";
 import Header from "./UI/components/Header/Header";
 import MainContent from "./UI/components/MainContent/MainContent";
-import ModulWindow from "./UI/Elements/ModalWindow/ModulWindow";
 
 function App() {
-
-
-
-
-
   return (
     <ContentProvider>
       <div className="wrapper">
